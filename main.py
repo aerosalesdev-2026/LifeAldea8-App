253fead6403d8efe917f5aa04db39548c01dfff4
alejandroesp <alejandroesp@github.com>
2026-08-02 22:04:28 -0500

    Configurar Flet para modo main

--- main.py
+++ main.py
@@ -10,53 +10,7 @@ async def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-
-    # Revisar actualización
-    nueva_version = actualizador.comprobar_actualizacion()
-
-
-    if nueva_version:
-
-        def actualizar(e):
-
-            ruta_apk = actualizador.descargar_apk(page,nueva_version["apk"])
-
-            print("APK descargado:", ruta_apk)
-
-
-        dialog = ft.AlertDialog(
-
-            title=ft.Text(
-                "Nueva actualización disponible"
-            ),
-
-            content=ft.Text(
-                nueva_version["mensaje"]
-            ),
-
-            actions=[
-
-                ft.ElevatedButton(
-                    "Actualizar",
-                    on_click=actualizar
-                )
-
-            ]
-
-        )
-
-
-        page.dialog = dialog
-
-        dialog.open = True
-
-        page.update()
-
-
-
-    # Continuar con la app
-    if not nueva_version:
-     router.configurar_router(page)
+    router.configurar_router(page)
 
 print("ANTES DE INICIAR FLET")
 
