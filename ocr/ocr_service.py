9bb6da4e0de69feb69498989391f0f036f3100df
alejandroesp <alejandroesp@github.com>
2026-08-02 21:55:27 -0500

    Cargar PaddleOCR bajo demanda

--- ocr/ocr_service.py
+++ ocr/ocr_service.py
@@ -3,21 +3,35 @@ from paddleocr import PaddleOCR
 from ocr.extractor import extraer_datos
 
 
-ocr = PaddleOCR(
-    lang="es"
-)
+ocr = None
+
+
+def obtener_ocr():
+    global ocr
+
+    if ocr is None:
+        print("Inicializando PaddleOCR...")
+
+        ocr = PaddleOCR(
+            lang="es"
+        )
+
+        print("PaddleOCR listo")
+
+    return ocr
 
 
 def procesar_imagen(ruta_imagen):
 
-    resultado = ocr.predict(
+    motor_ocr = obtener_ocr()
+
+    resultado = motor_ocr.predict(
         ruta_imagen
     )
 
     textos = []
 
     for r in resultado:
-
         textos.extend(
             r["rec_texts"]
         )
